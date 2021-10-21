caf4780cb1f4110a8e3abdd5d0a62b49affbb724
Saci <saciperere@riseup.net>
2021-10-21 19:25:43 -0300

    clean up

--- backend/app/app/schemas/dublin_points.py
+++ backend/app/app/schemas/dublin_points.py
@@ -1,35 +1,38 @@
 #!/usr/bin/env ipython
 
-from typing import Optional
-from pydantic import BaseModel
 from datetime import datetime
+from typing import Optional
+
+from pydantic import BaseModel
+
 
 # Shared properties
 class DublinPointsBase(BaseModel):
-    id : Optional[int]
-    trajectory_id : Optional[int]
-    instant : Optional[datetime]
-    lat : Optional[float]
-    lng : Optional[float]
-    lat_mercator : Optional[float]
-    lng_mercator : Optional[float]
-    speed : Optional[float]
-    acceleration : Optional[float]
-    position : Optional[float]
-    delta_dist : Optional[float]
-    delta_time : Optional[float]
-    bearing : Optional[float]
-    year : Optional[int]
-    month : Optional[int]
-    day : Optional[int]
-    hour : Optional[int]
-    minute : Optional[int]
-    stop_labels : Optional[bool]
+    id: Optional[int]
+    trajectory_id: Optional[int]
+    instant: Optional[datetime]
+    lat: Optional[float]
+    lng: Optional[float]
+    lat_mercator: Optional[float]
+    lng_mercator: Optional[float]
+    speed: Optional[float]
+    acceleration: Optional[float]
+    position: Optional[float]
+    delta_dist: Optional[float]
+    delta_time: Optional[float]
+    bearing: Optional[float]
+    year: Optional[int]
+    month: Optional[int]
+    day: Optional[int]
+    hour: Optional[int]
+    minute: Optional[int]
+    stop_labels: Optional[bool]
 
 
 # Properties to receive on item creation
 class DublinPointsCreate(DublinPointsBase):
     pass
+
 
 # Properties to receive on item update
 class DublinPointsUpdate(DublinPointsBase):
@@ -45,6 +48,7 @@ class DublinPointsInDBBase(DublinPointsBase):
 # Properties to return to client
 class DublinPoints(DublinPointsInDBBase):
     pass
+
 
 # Properties properties stored in DB
 class DublinPointsInDB(DublinPointsInDBBase):
